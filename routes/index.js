9761b828182bec1e1004409a66d584e1e395b5f4
Krishna-Koyyalamudi <krishna.kks3797@gmail.com>
2020-09-29 19:53:28 -0500

    add node_modules

--- routes/index.js
+++ routes/index.js
@@ -9,15 +9,25 @@
 // module.exports = router;
 
 
+// const express = require('express');
+// const router  = express.Router();
+// //login page
+// router.get('/', (req,res)=>{
+//     res.render('welcome');
+// })
+// //register page
+// router.get('/register', (req,res)=>{
+//     res.render('register');
+// })
+
 const express = require('express');
-const router  = express.Router();
-//login page
-router.get('/', (req,res)=>{
-    res.render('welcome');
-})
-//register page
-router.get('/register', (req,res)=>{
-    res.render('register');
-})
+const router = express.Router();
+
+// GET home page.
+router.get('/', function(req, res) {
+  res.redirect('/catalog');
+});
+
+module.exports = router;
 
 module.exports = router; 
