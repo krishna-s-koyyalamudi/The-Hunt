018299d639e92beca8856ab60cbf4b54bb3b9660
Vikas2004 <S538336@NWMISSOURI.EDU>
2020-09-30 00:49:28 -0500

    database.js commit

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const bodyparser = require("body-parser");
 const app = express();
+var db =  require("./database.js")
 
 app.use(express.static("public"));
 
@@ -14,6 +15,21 @@ app.get('/', (req, res) => {
 
 
 app.use(bodyparser.json({type:"application/*+json"}));
+
+app.get("/api/users", (req, res, next) => {
+  var sql = "select * from user"
+  var params = []
+  db.all(sql, params, (err, rows) => {
+      if (err) {
+        res.status(400).json({"error":err.message});
+        return;
+      }
+      res.json({
+          "message":"success",
+          "data":rows
+      })
+    });
+});
 
 app.listen(8000, () => {
   console.log('Example app listening on port 8000!')
