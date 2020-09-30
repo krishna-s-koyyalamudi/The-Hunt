b4f05c5933ad3d04c1e07cc3955de8d6403453dd
Krishna-Koyyalamudi <krishna.kks3797@gmail.com>
2020-09-29 22:28:16 -0500

    Project Refresh

--- index.js
+++ index.js
@@ -1,32 +1,16 @@
 const express = require('express')
-const path = require('path')
-
-const bodyParser = require("body-parser")
-
+const bodyparser = require("body-parser");
 const app = express();
-//app.use(express.staticProvider(__dirname + '/public'));
-
-app.use(express.static('public'))
-// app.use(express.static('files'))
-//app.use('/static', express.static('public'))
-//app.use('/static', express.static(path.join(__dirname, 'public')))
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + 'views\Login\Login.html')
+  //res.render('/index')
+  res.sendFile(__dirname + '\\views\\Login\\Login.html')
 });
 
-app.post('/', function (req, res) {
-    res.send('Got a POST request')
-  });
-
-app.put('/user', function (req, res) {
-    res.send('Got a PUT request at /user')
-  });
-
-app.delete('/user', function (req, res) {
-    res.send('Got a DELETE request at /user')
-  })
+app.use(bodyparser.json({type:"application/*+json"}));
 
 app.listen(8000, () => {
   console.log('Example app listening on port 8000!')
 });
+
+//app.post("/addcustomer", jsonparser, customer.addcustomer);
