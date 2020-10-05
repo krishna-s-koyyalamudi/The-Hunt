e17fcd9947cbf3c02e0a56fdd357c97fcd9a45a4
Deekshith Maram <s538365@nwmissouri.edu>
2020-10-05 13:47:07 -0500

    add dashboard layout

--- index.js
+++ index.js
@@ -1,20 +1,45 @@
 const express = require('express')
+const engines = require('consolidate')
+const path = require('path')
+const ejsLint = require('ejs-lint');
 const bodyparser = require("body-parser");
 const app = express();
 var db = require("./database.js")
 
-app.set('view engine', 'pug');
+// set the root view folder
+// app.set('views', path.join(__dirname, 'views'))
+
+// specify desired view engine (EJS)
+app.set('view engine', 'ejs')
+// app.set('view engine', 'pug');
+app.engine('ejs', engines.ejs)
 
 app.use(express.static("public"));
 
-port = process.env.PORT || 80
+port = process.env.PORT || 3000
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/Login.html')
 });
 
 app.get('/dashboard', (req, res)=> {
-  res.render(__dirname + '/views')
+  res.render(__dirname + '/views/layout.ejs')
+})
+
+app.get('/home', (req, res)=> {
+  res.render(__dirname + '/views/home.ejs')
+})
+
+app.get('/leaderboard', (req, res)=> {
+  res.render(__dirname + '/views/leaderboard.ejs')
+})
+
+app.get('/createquest', (req, res)=> {
+  res.render(__dirname + '/views/createQuest.ejs')
+})
+
+app.get('/profile', (req, res)=> {
+  res.render(__dirname + '/views/profile.ejs')
 })
 
 app.get("/register", (req, res) => {
