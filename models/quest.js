dc35ec73698a6a2e1a1c508b3f77ec250536c9da
nrajubn <60019513+nrajubn@users.noreply.github.com>
2020-10-06 18:24:34 -0500

    Created models/quest.js and models/team.js

--- models/quest.js
+++ models/quest.js
@@ -1,1 +1,65 @@
 
+/**
+ * Quest Model
+ * Creatiion of quests
+ * 
+ * @author nookaRaju
+ */
+
+var quest = {};
+module.exports = (sequelize, DataTypes) => {
+    quest = sequelize.define('quest', {
+       questId: {
+           type: DataTypes.INTEGERS,
+           primaryKey: true,
+           allowIncrement: true
+       },
+       questName: {
+           type: DataTypes.STRING,
+           allowNull: false
+       },
+       creatorUserId: {
+        type: DataTypes.INTEGER,
+        references: {
+            model: "user",
+            primaryKey: "userid",}
+
+        },
+        dateTimeCreated: {
+            type: DataTypes.DATE,
+            get() {
+                return moment(this.getDataValue('dateTimeCreated')).format('MM/DD/YYYY');
+            }
+        },
+        dateTimeLastEdited: {
+            type: DataTypes.DATE,
+            get(){
+                return moment(this.getDataValue('dateTimeLastEdited')).format('MM/DD/YYYY');
+
+            }
+
+        },
+        latitude: {
+            type: Sequelize.INTEGER,
+            allowNull: true,
+            defaultValue: null,
+            validate: { min: -90, max: 90 }
+          },
+          longitude: {
+            type: Sequelize.INTEGER,
+            allowNull: true,
+            defaultValue: null,
+            validate: { min: -180, max: 180 }
+          },
+        }, {
+          validate: {
+            bothCoordsOrNone() {
+              if ((this.latitude === null) !== (this.longitude === null)) {
+                throw new Error('Require either both latitude and longitude or neither')
+              }
+            }
+          }
+
+    });
+   module.exports = quest;
+}
--- models/team.js
+++ models/team.js
@@ -1,1 +1,53 @@
+/**
+ * Team Model
+ * It states the details fucntionality of team
+ * 
+ * @author Nooka Raju
+ * 
+ */
+
+
+
+var team = {};
+module.exports = (sequelize, DataTypes) => {
+    team= sequelize.define('team', {
+        teamId: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            allowIncrement: true
+            
+        },
+        teamName: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        creatorUserId: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: "user",
+                primaryKey: "userid",}
+
+            },
+            dateTimeCreated: {
+                type: DataTypes.DATE,
+                get() {
+                    return moment(this.getDataValue('dateTimeCreated')).format('MM/DD/YYYY');
+                }
+            },
+            dateTimeLastEdited: {
+                type: DataTypes.DATE,
+                get(){
+                    return moment(this.getDataValue('dateTimeLastEdited')).format('MM/DD/YYYY');
+
+                }
+
+            },
+           
+           
+
+    });
+    module.exports = team;
+
+}
+
 
