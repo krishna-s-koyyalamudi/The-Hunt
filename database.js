107ca555190f384eebd8f25d3e55e2cbae70b33b
Vikas2004 <S538336@NWMISSOURI.EDU>
2020-10-06 14:38:14 -0500

    error fixes in database.js and models/user.js commit

--- database.js
+++ database.js
@@ -1,44 +1,42 @@
 const sqlite3 = require('sqlite3').verbose();
 var md5 = require('md5')
 
-// open database in local disk
-let db = new sqlite3.Database('C:/sqlite/hunt.db', sqlite3.OPEN_READWRITE,(err) => {
+// create database in local disk
+let db = new sqlite3.Database('./hunt.db',(err) => {
     if (err) {
     return console.error(err.message);
     }
+    else{
     console.log('Connected to the hunt database.');  
-    
-        db.run(`CREATE TABLE IF NOT EXISTS user (
-            email VARCHAR(30) ,
-            password VARCHAR(8) ,
-            userId INT(10) PRIMARY KEY NOT NULL UNIQUE AUTO INCREMENT,
-            userName VARCHAR(15) ,
-            dateCreated DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-            dateLastAccessed DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-             )`,
-
-             (err) => {
-              if (err) {
-                  // Table already created
-                  console.log('User table already exists.')
-              }else{
-                  // Table just created, creating some rows
-                  var insert = 'INSERT INTO user (email, password,userName) VALUES (?,?,?)'
-                  db.run(insert, ["ravuluri@gmail.com",md5("ravi1432"),"ravi"])
-                  db.run(insert, ["anusha12@gmail.com",md5("anus1233"),"anuravi"])
-                  db.run(insert, ["sandy@gmail.com",md5("sandy123"),"sandy12"])
-                  db.run(insert, ["devi@gmail.com",md5("devi4590"),"devi45"])
-                  db.run(insert, ["tarun@gmail.com",md5("tarun231"),"tarunfarhen"])
-                  db.run(insert, ["aswin@gmail.com",md5("aswin456"),"aswinrebeca"])
-                  db.run(insert, ["raghu@gmail.com",md5("raghu2345"),"raghunikki"])
-                  db.run(insert, ["eswar@gmail.com",md5("eswar546"),"eswarfox"])
-              }
-          });  
+        db.run('CREATE TABLE IF NOT EXISTS user (email text,password text,userId INTEGER PRIMARY KEY AUTOINCREMENT,userName text)');
+        
+        db.run ('CREATE TABLE IF NOT EXISTS team (teamId INTEGER PRIMARY KEY AUTOINCREMENT  , creatorUserId INTEGER, FOREIGN KEY(creatorUserId) REFERENCES user(userId))');
+        
+    }
+            // Table just created, creating some rows
+            let sql = 'INSERT INTO user (email, password,userName) VALUES (?,?,?)';
+            db.run(sql, ["ravuluri@gmail.com", md5("ravi1432"), "ravi"]);
+            
+            db.run(sql, ["anusha12@gmail.com", md5("anus1233"), "anuravi"]);
+            
+            db.run(sql, ["sandy@gmail.com", md5("sandy123"), "sandy12"]);
+            
+            db.run(sql, ["devi@gmail.com", md5("devi4590"), "devi45"]);
+            
+            db.run(sql, ["tarun@gmail.com", md5("tarun231"), "tarunfarhen"]);
+            
+            db.run(sql, ["aswin@gmail.com", md5("aswin456"), "aswinrebeca"]);
+            
+            db.run(sql, ["raghu@gmail.com", md5("raghu2345"), "raghunikki"]);
+            
+            db.run(sql, ["eswar@gmail.com", md5("eswar546"), "eswarfox"]);
+            
         });
-       
+
+        module.export = db
+        
+
           // close the database connection
-    // db.close((err) => {  
-    //     if (err) { 
-    //     return console.error(err.message); }
-    //     console.log('Close the database connection.');
-    //     });
+    // db.close();
+    // console.log('Close the database');
+    
---
+++ models/user.js
@@ -0,0 +1,17 @@
+var express = require("express")
+var app = express()
+const db = require('../database.js')
+app.get("/api/users", (req, res, next) => {
+    var sql = "select * from user"
+    var params = []
+    db.all(sql, params, (err, rows) => {
+        if (err) {
+          res.status(400).json({"error":err.message});
+          return;
+        }
+        res.json({
+            "message":"success",
+            "data":rows
+        })
+      });
+});
