335db90f4ee288a27229d03a4b74b7c57b8445ef
Puneeth159 <60018781+Puneeth159@users.noreply.github.com>
2020-10-09 20:16:45 -0500

    teamController.js added

--- controllers/teamController.js
+++ controllers/teamController.js
@@ -1,1 +1,29 @@
+const express=require('express')
+const api=express.Router()
+const Model=require('../models/team.js')
 
+
+app.get("/api/team", (req, res, next) => {
+    var sql = "select * from team"
+    var params = []
+    db.all(sql, params, (err, rows) => {
+      if (err) {
+        res.status(400).json({ "error": err.message });
+        return;
+      }
+      res.json({
+        "message": "success",
+        "data": rows
+      })
+    });
+  });
+app.post("/api/team/", (req, res, next) => {
+    var errors = []
+    if (!req.body.teamnname) {
+      errors.push("Team name not specified");
+    }   
+    if (errors.length) {
+      res.status(400).json({ "error": errors.join(",") });
+      return;
+    }
+  
