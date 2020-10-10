6a47282963cd499736c18bd488a4cd18b0204228
Ravichanderreddy-goli <60166223+Ravichanderreddy-goli@users.noreply.github.com>
2020-10-09 23:06:41 -0500

    Update playerScore.js

---
+++ models/playerScore.js
@@ -0,0 +1,14 @@
+CREATE TABLE IF NOT EXISTS playerScore (
+    `playerScoreId` INT,
+    `playerId` INT,
+    `locationId` INT
+);
+INSERT INTO playerScore VALUES
+    (111,911,666),
+    (222,912,345),
+    (333,913,234),
+    (444,914,890),
+    (555,915,776),
+    (666,916,678),
+    (777,917,509),
+    (888,918,980);
