83ca27fbd78ae9434ebcf46a42726e1f63b7e674
pruthvi-naskanti <pruthvinaskanti@gmail.com>
2020-10-09 22:26:58 -0500

    defined response status for different inputs given.

--- controllers/questController.js
+++ controllers/questController.js
@@ -65,15 +65,27 @@ app.post("/api/quest/location", (req, res, next) => {
 
 
 app.post("/api/quest/quest_location", (req, res, next) => {
-            var errors = []
-            if (!req.body.latitude) {
-                errors.push("No latitude specified");
-            }
-            if (!req.body.longitude) {
-                errors.push("No longitude specified");
-            }
-            var location = {
-                latitude: req.body.latitude,
-                longitude: req.body.longitude,
-            }
-            var sql = 'INSERT INTO quest (Quest location Latitude, Quest location Longitude) VALUES (?,?)'
+    var errors = []
+    if (!req.body.latitude) {
+        errors.push("No latitude specified");
+    }
+    if (!req.body.longitude) {
+        errors.push("No longitude specified");
+    }
+    var location = {
+        latitude: req.body.latitude,
+        longitude: req.body.longitude,
+    }
+    var sql = 'INSERT INTO quest (Quest location Latitude, Quest location Longitude) VALUES (?,?)'
+    var params = [data.latitude, data.longitude]
+    db.run(sql, params, function(err, result) {
+        if (err) {
+            res.status(400).json({ "error": err.message })
+            return;
+        }
+        res.json({
+            "message": "success",
+            "data": data
+        })
+    });
+})
