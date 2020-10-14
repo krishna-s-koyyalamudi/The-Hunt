f6fbff3e1d20f1f2a5a6a00024ee9b7e9c398521
Rajeshwari-Rudra <60014358+Rajeshwari-Rudra@users.noreply.github.com>
2020-10-14 09:58:23 -0400

    updating

--- models/location.js
+++ models/location.js
@@ -1,1 +1,26 @@
-
+/**
+*  location model
+*  explains each attribute in location entity.
+*
+* @author Rajeshwari Rudravaram
+* 
+*
+*/
+'use strict';
+var location = {};
+module.exports = (sequelize, DataTypes) => {
+    location= sequelize.define('location', {
+        locationId: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+             notEmpty: true
+        },
+     questId: {
+            type: DataTypes.INTEGER,
+            allowNull: false
+        },{}); 
+               location.associate = function(models) {
+              // associations can be defined here
+                     };
+               return location;
+               };
