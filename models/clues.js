4d89a06f738f48d283aca0eb26dace80d68e3e2b
nrajubn <60019513+nrajubn@users.noreply.github.com>
2020-10-14 11:12:58 -0500

    Added clues.js

--- models/clues.js
+++ models/clues.js
@@ -1,1 +1,35 @@
-      
+      /**
+ * Clues Model
+ * Creatiion of Clues
+ * 
+ * @author nookaRaju
+ */
+
+var clues = {};
+module.exports = (sequelize, DataTypes) => {
+    clues = sequelize.define('clues', {
+        clueId: {
+            type: DataTypes.INTEGERS,
+            primaryKey: true,
+            allowIncrement: true
+        },
+locationId: {
+    type: this.DataTypes.INTEGERS,
+   references: {
+       Model: "location",
+       primaryKey: "locationId"
+   },
+
+   clueString : {
+       type: this.DataTypes.String,
+       allowNull: false
+   
+   },
+   clueSortOrder: {
+       type: this.DataTypes.INTEGERS,
+       allowNull: false,
+       order: ['clueSortOrder', 'ASC'] // Sorts by COLUMN_NAME in ascending order
+   }
+}
+
+}
