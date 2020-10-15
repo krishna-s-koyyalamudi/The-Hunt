e0f39014438730b773e59351f198e3a8a4524d87
Rajeshwari_Rudra <rajrudra.raj@gmail.com>
2020-10-15 18:05:04 -0400

    modifying

--- models/competition.js
+++ models/competition.js
@@ -40,7 +40,8 @@ module.exports = (sequelize, DataTypes) => {
                 get(){
                     return moment(this.getDataValue('endDateTime')).format('MM/DD/YYYY hh:mm:ss');
                 }
-                },{}); 
+                },
+            }); 
                competition.associate = function(models) {
               // associations can be defined here
                      };
