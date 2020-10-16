6ddec6921b0b185d534e09c7b9616e7d329f3ad2
Chitipolu Sri Sudheera <csrisudheera96@gmail.com>
2020-10-16 06:26:45 -0500

    updated cypress sign up page

--- cypress/integration/examples/sample_register_signup.spec.js
+++ cypress/integration/examples/sample_register_signup.spec.js
@@ -10,24 +10,27 @@ Cypress.on('uncaught:exception', (err, runnable) => {
     return false
   })
     
-    it('Does not do much!',function() {
+    it('Register!',function() {
       cy.visit('https://hunt2020.herokuapp.com/Register') // Opens URL
     
-      cy.get('#uname').type('csrisudheera96@gmail.com') // Gives Email 
+      cy.get('#email').type('csrisudheera96@gmail.com') // Gives Email 
 
       cy.get('#password').type('qwert@123') // Gives New password
 
       cy.get('#newpassword').type('qwert@123') // Gives Conform password
 
-    cy.get('#signup').click() // Clicks on register
+    cy.get('.rounded-100.text-center').click() // Clicks on register
 
     cy.wait(3000) // waits for going to other page after clicking register
 
-    cy.get('#login').click() // Clicks on login
+    cy.visit('https://hunt2020.herokuapp.com/Register') // Opens URL
+    cy.wait(3000) // Waits for visiting login page
+
+
+    cy.get('#Login').click() // Clicks on login
 
     cy.wait(3000) // Waits for visiting login page
 
-    cy.visit('https://hunt2020.herokuapp.com/Login') // Opens login page URL
       
     
     })
