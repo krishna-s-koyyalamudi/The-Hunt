1efeff488249fba0cad868062461e1c9afdc2b73
sumanareddy <s538360@nwmissouri.edu>
2020-10-15 20:26:27 -0500

    added cypress test file for Create Competition page

---
+++ cypress/integration/examples/sample_create_competition.spec.js
@@ -0,0 +1,18 @@
+
+/// <reference types="Cypress"/>
+
+ describe('My First Test for Competition page', function(){
+    it('Verify input types &Buttons',function()  {
+      cy.visit('https://hunt2020.herokuapp.com/create_competition')
+      cy.url().should('include','competition')
+      cy.get('input[class="no-outline"]').should('be.visible').should('be.enabled').type('Enter Competition')
+      cy.get('#date').type('2020-10-21')
+      cy.get('#time').type("08:20")
+      cy.get('input[placeholder = "End Date"]').type('2020-10-21')
+      cy.get('input[placeholder = "End Time"]').type("08:20")     
+      cy.get('button[data-toggle=dropdown]').should('be.visible').click()
+
+     
+    })
+})
+  
