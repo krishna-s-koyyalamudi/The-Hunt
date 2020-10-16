6d9dde5636da22c2f6682d9f4c882c1ad2c0bd21
Rajeshwari-Rudra <60014358+Rajeshwari-Rudra@users.noreply.github.com>
2020-10-16 19:11:11 -0400

    updating attributes

--- models/location.js
+++ models/location.js
@@ -27,8 +27,21 @@ module.exports = (sequelize, DataTypes) => {
             defaultValue: null,
             validate: { min: -90, max: 90 }
           },
-          
-    });
+        longitude: {
+            type: Sequelize.INTEGER,
+            allowNull: true,
+            defaultValue: null,
+            validate: { min: -180, max: 180 }
+          },
+         {
+          validate: {
+            bothCoordsOrNone() {
+              if ((this.latitude === null) !== (this.longitude === null)) {
+                throw new Error('Require either both latitude and longitude or neither')
+              }
+            }
+          }
+});
       location.associate = function(models) {
            // associations can be defined here
         };
