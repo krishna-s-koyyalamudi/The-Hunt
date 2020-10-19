484205c52d5f954fbd33bfe3971dbbe9eb77adce
Vikas2004 <S538336@NWMISSOURI.EDU>
2020-10-19 02:21:37 -0500

    cleaned the hunt.db and ready for authentication

--- database.js
+++ database.js
@@ -15,31 +15,18 @@ let sqldb = new sqlite3.Database(DBSOURCE, (err) => {
         console.log('Connected to the hunt database.');
     }
     // seed relational data
-    sqldb.run('CREATE TABLE IF NOT EXISTS user (email TEXT PRIMARY KEY,password text)');
-    console.log("Table created.")
-    // Table just created, creating some rows
-    let sql = 'INSERT INTO user (email, password) VALUES (?,?)';
-    sqldb.run(sql, ["ravuluri@gmail.com", md5("ravi1432")]);
-    sqldb.run(sql, ["anusha12@gmail.com", md5("anus1233")]);
-    sqldb.run(sql, ["sandy@gmail.com", md5("sandy123")]);
-    sqldb.run(sql, ["devi@gmail.com", md5("devi4590")]);
-    sqldb.run(sql, ["tarun@gmail.com", md5("tarun231")]);
-    sqldb.run(sql, ["aswin@gmail.com", md5("aswin456")]);
-    sqldb.run(sql, ["raghu@gmail.com", md5("raghu2345")]);
-    sqldb.run(sql, ["eswar@gmail.com", md5("eswar546")]);
-    sqldb.run(sql, ["dcase@nwmissouri.edu", md5("dcase")]);
-    console.log("data inserted.")
-
+    sqldb.run('CREATE TABLE IF NOT EXISTS user (email text,password text, userId PRIMARY KEY AUTOINCREMENT)');
+    console.log("Table user created.")
     sqldb.run('CREATE TABLE IF NOT EXISTS team (teamId INTEGER PRIMARY KEY AUTOINCREMENT, teamName text, creatorUserId INTEGER, FOREIGN KEY (creatorUserId) REFERENCES user(userId))');
-    console.log("Table 2 created.")
+    console.log("Table team created.")
     let sql1 = 'INSERT INTO team (teamName) VALUES (?)';
     sqldb.run(sql1, ["sunRisers"]);
     sqldb.run(sql1, ["royalChallengers"]);
-    console.log("data 2 inserted")
+    console.log("data into team inserted")
     sqldb.run('CREATE TABLE IF NOT EXISTS quest(questId INTEGER PRIMARY KEY AUTOINCREMENT, questName text, questCreatorUserId INTEGER,longitude text, latitude text, FOREIGN KEY (questCreatorUserId) REFERENCES user(userId))');
-    console.log("table 3 created")
+    console.log("table quest created")
     sqldb.run('CREATE TABLE IF NOT EXISTS competition(competitionId INTEGER PRIMARY KEY AUTOINCREMENT, competitionName text, competitionCreatorUserId INTEGER, competitionQuestId INTEGER, startDateTime TEXT, endDateTime TEXT,FOREIGN KEY (competitionCreatorUserId) REFERENCES user(userId), FOREIGN KEY (competitionQuestId) REFERENCES quest(questId))');
-    console.log("table 4 created")
+    console.log("table competition created")
 });
 
 // // close the database connection
