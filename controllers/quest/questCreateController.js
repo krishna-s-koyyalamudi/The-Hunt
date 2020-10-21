23589c43da6bba05c059d9605bd1eeef76b461d2
Rohith Chittimalla <S538308@nwmissouri.edu>
2020-10-21 08:53:16 -0500

    assigning rohith chittimalla to quest/create controller

---
+++ controllers/quest/questCreateController.js
@@ -0,0 +1,10 @@
+/**
+ * This will handle the request for quest/create
+ * 
+ * get - returns the page for creating a new quest
+ * 
+ * post - it cretes the team in the data store 
+ * 
+ * @author Rohith Chittimalla <S538308@nwmissouri.edu>
+ * 
+ */
