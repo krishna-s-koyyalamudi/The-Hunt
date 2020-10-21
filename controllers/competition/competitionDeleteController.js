c4bb291099ba7a46d49a32a7d0eaf9c67082c449
Chitipolu Sri Sudheera <csrisudheera96@gmail.com>
2020-10-21 15:20:32 -0500

    assigning Sumana, Sudheera for competition/delete

---
+++ controllers/competition/competitionDeleteController.js
@@ -0,0 +1,11 @@
+/**
+ * This will handle the request for competition/delete
+ * 
+ * get - returns the page for deleting a competition
+ * 
+ * post - it deletes the team in the data store 
+ * 
+ * @author Reddybathula,Sumana Reddy <s538360@nwmissouri.edu>
+ * @author Sri Sudheera Chitipolu <S538422@nwmissouri.edu>
+ * 
+ */ 
