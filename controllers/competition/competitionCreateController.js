ae0110261dbce8ae976938869e9f8bcf5bb35723
Chitipolu Sri Sudheera <csrisudheera96@gmail.com>
2020-10-21 15:17:55 -0500

    assigning Sumana, Sudheera to competition/create

---
+++ controllers/competition/competitionCreateController.js
@@ -0,0 +1,11 @@
+/**
+ * This will handle the request for competition/create
+ * 
+ * get - returns the page for creating a new competition
+ * 
+ * post - it creates the team in the data store 
+ * 
+ * @author Reddybathula,Sumana Reddy <s538360@nwmissouri.edu>
+ * @author Sri Sudheera Chitipolu <S538422@nwmissouri.edu>
+ * 
+ */ 
