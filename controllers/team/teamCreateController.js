116a5fce91f2e5dc779d1ff66a942bf57c7b5860
Bhaskar <reddybhaskar437@gmail.com>
2020-10-22 17:42:11 -0500

    created team/teamCreateController.js

---
+++ controllers/team/teamCreateController.js
@@ -0,0 +1,13 @@
+/**
+ * This will handle the request for team/create
+ * 
+ * get - returns the page for creating a new team
+ * 
+ * post - it cretes the team in the data store 
+ * 
+ * @author Bhaskar Reddy Minupuri <S538310@nwmissouri.edu>
+ * 
+ */
+
+
+
