135f7e38e92813922116c0ab2c2f0c7e69588787
Krishna-Koyyalamudi <krishna.kks3797@gmail.com>
2020-10-22 23:31:05 -0500

    add patch method to quest/questEditController.js

--- controllers/quest/questEditController.js
+++ controllers/quest/questEditController.js
@@ -1,3 +1,33 @@
 /*
 * @author Krishna Sumanth Koyyalamudi
 */
+
+const express = require('express');
+const app = express.Router();
+ 
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: true }));
+var jsonParser = bodyParser.json()
+ 
+ app.patch("/api/quest/:id", (req, res, next) => {
+    var data = {
+      questName: req.body.questName,
+      questid: req.body.questid
+    }
+    db.run(
+      `UPDATE quest set 
+           questName = coalesce(?,questName), 
+           qurestId = COALESCE(?,questid), 
+           WHERE questId = ?`,
+      [data.questName, data.questId, req.params.questId],
+      (err, result) => {
+        if (err) {
+          res.status(400).json({ "error": res.message })
+          return;
+        }
+        res.json({
+          message: "success",
+          data: data
+        })
+      });
+  })
