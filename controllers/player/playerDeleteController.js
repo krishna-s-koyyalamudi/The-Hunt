800a4c7b96503a483121d11f99c8302f7d9fdd71
chandu polisetty <chandrakanthpolisetty@gmail.com>
2020-10-23 18:35:54 -0500

    Added playerDeleteController.js

---
+++ controllers/player/playerDeleteController.js
@@ -0,0 +1,48 @@
+/*
+* @author Chandrakanth Polisetty
+*/
+const express = require('express');
+const app = express.Router();
+
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: true }));
+var jsonParser = bodyParser.json()
+
+
+
+app.delete("/player/delete", jsonParser, (req, res, next) => {
+    console.log(req, " ----request is here")
+    let playerId = req.body;
+
+    let error = null;
+    sqldb.get('DELETE FROM player WHERE id = ? ', [playerId], (err, response) => {
+        if (err) {
+            console.error('Unable to delete palyer', err);
+            error = 'Unable to delete player';
+        } else if (!err && !response) {
+            console.log("player not found")
+        } else {
+            //   res.render('./partials/home');
+            console.log("player Deleted successfully")
+        }
+        console.log(err, res);
+    })
+})
+
+async function del(req, res, next) {
+    try {
+        const id = parseInt(req.params.id, 10);
+
+        const success = await employees.delete(id);
+
+        if (success) {
+            res.status(204).end();
+        } else {
+            res.status(404).end();
+        }
+    } catch (err) {
+        next(err);
+    }
+}
+
+module.exports = app;
