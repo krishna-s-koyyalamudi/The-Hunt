e6fa562504dd1c1e24d8f01bd1c9ebeb5e720c67
Ravichanderreddy-goli <60166223+Ravichanderreddy-goli@users.noreply.github.com>
2020-10-22 19:23:08 -0500

    Update teamEditController.js

--- controllers/team/teamEditController.js
+++ controllers/team/teamEditController.js
@@ -1,11 +1,1 @@
 
-/**
- * This will handle the request for team/edit
- * 
- * get - returns the page for edting the team
- * 
- * post - it edits the team in the data store 
- * 
- * @author Ravichander Reddy Goli <S538386@nwmissouri.edu>
- *
- */
