72c682d547d8bfc3d8faa5e7e014367f7b52544d
Yashwanth <yashwanthdamera6@gmail.com>
2020-10-22 20:47:44 -0500

    updated team/teamEditController.js

--- controllers/team/teamEditController.js
+++ controllers/team/teamEditController.js
@@ -1,1 +1,33 @@
+/**
+ *  @author Venkatayashwanth Damera
+ */
 
+const express = require('express');
+const app = express.Router();
+ 
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: true }));
+var jsonParser = bodyParser.json()
+ 
+ app.patch("/api/team/:id", (req, res, next) => {
+    var data = {
+      teamName: req.body.teamName,
+      teamid: req.body.teamid
+    }
+    db.run(
+      `UPDATE team set 
+           teamName = coalesce(?,teamName), 
+           teamId = COALESCE(?,teamid), 
+           WHERE teamId = ?`,
+      [data.teamName, data.teamId, req.params.teamId],
+      (err, result) => {
+        if (err) {
+          res.status(400).json({ "error": res.message })
+          return;
+        }
+        res.json({
+          message: "success",
+          data: data
+        })
+      });
+  })
