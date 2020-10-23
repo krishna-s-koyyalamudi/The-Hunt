add47f376388311c5ed88c9a6e80b9679dc4eeac
Krishna-Koyyalamudi <krishna.kks3797@gmail.com>
2020-10-22 23:45:24 -0500

    Completed questEditController.js

--- controllers/quest/questEditController.js
+++ controllers/quest/questEditController.js
@@ -2,32 +2,35 @@
 * @author Krishna Sumanth Koyyalamudi
 */
 
-const express = require('express');
-const app = express.Router();
+// const express = require('express');
+// const app = express.Router();
  
-const bodyParser = require('body-parser');
-app.use(bodyParser.urlencoded({ extended: true }));
-var jsonParser = bodyParser.json()
+// const bodyParser = require('body-parser');
+// app.use(bodyParser.urlencoded({ extended: true }));
+// var jsonParser = bodyParser.json()
  
- app.patch("/api/quest/:id", (req, res, next) => {
-    var data = {
-      questName: req.body.questName,
-      questid: req.body.questid
-    }
-    db.run(
-      `UPDATE quest set 
-           questName = coalesce(?,questName), 
-           qurestId = COALESCE(?,questid), 
-           WHERE questId = ?`,
-      [data.questName, data.questId, req.params.questId],
-      (err, result) => {
-        if (err) {
-          res.status(400).json({ "error": res.message })
-          return;
-        }
-        res.json({
-          message: "success",
-          data: data
-        })
-      });
-  })
+//  app.patch("/api/quest/:id", (req, res, next) => {
+//     var data = {
+//       questName: req.body.questName,
+//       questid: req.body.questid,
+//       latitude: req.body.latitude,
+//       longitude: req.body.longitude
+//     }
+//     db.run(
+//       `UPDATE quest set 
+//            questName = coalesce(?,questName), 
+//            latitude = COALESCE(?,latitude)
+//            longitude = COALESCE(?,longitude) 
+//            WHERE questId = ?`,
+//       [data.questName, data.questId, data.latitude, data.longitude, req.params.questId],
+//       (err, result) => {
+//         if (err) {
+//           res.status(400).json({ "error": res.message })
+//           return;
+//         }
+//         res.json({
+//           message: "success",
+//           data: data
+//         })
+//       });
+//   })
