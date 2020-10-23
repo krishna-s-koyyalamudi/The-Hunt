0695d0501bcd756a6ac76a0749a56996171a068d
chandu polisetty <chandrakanthpolisetty@gmail.com>
2020-10-23 18:43:09 -0500

    delete player

--- controllers/player/playerDeleteController.js
+++ controllers/player/playerDeleteController.js
@@ -1,53 +1,3 @@
-<<<<<<< HEAD
-/*
-* @author Chandrakanth Polisetty
-*/
-const express = require('express');
-const app = express.Router();
-
-const bodyParser = require('body-parser');
-app.use(bodyParser.urlencoded({ extended: true }));
-var jsonParser = bodyParser.json()
-
-
-
-app.delete("/player/delete", jsonParser, (req, res, next) => {
-    console.log(req, " ----request is here")
-    let playerId = req.body;
-
-    let error = null;
-    sqldb.get('DELETE FROM player WHERE id = ? ', [playerId], (err, response) => {
-        if (err) {
-            console.error('Unable to delete palyer', err);
-            error = 'Unable to delete player';
-        } else if (!err && !response) {
-            console.log("player not found")
-        } else {
-            //   res.render('./partials/home');
-            console.log("player Deleted successfully")
-        }
-        console.log(err, res);
-    })
-})
-
-async function del(req, res, next) {
-    try {
-        const id = parseInt(req.params.id, 10);
-
-        const success = await employees.delete(id);
-
-        if (success) {
-            res.status(204).end();
-        } else {
-            res.status(404).end();
-        }
-    } catch (err) {
-        next(err);
-    }
-}
-
-module.exports = app;
-=======
 /**
  *@author Pruthvi Naskanti
  */
@@ -78,4 +28,3 @@ app.delete("/player/delete", jsonParser, (req, res, next) => {
 
     })
 })
->>>>>>> c2667256a14519dffee6fa5c01baa7546421d634
