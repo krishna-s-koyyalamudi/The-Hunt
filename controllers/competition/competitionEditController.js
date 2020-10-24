52ff1dbb4165fba87bc57ef51c8a2e0667204bc4
sumanthreddy1233 <sumanthreddy.naidu@gmail.com>
2020-10-23 22:17:11 -0500

    updted competitionEditController

--- controllers/competition/competitionEditController.js
+++ controllers/competition/competitionEditController.js
@@ -7,3 +7,29 @@
  * 
  * 
  */ 
+const express=require('express')
+const app=express.Router()
+const Model=require('../../models/competition.js')
+app.put()
+  app.patch("/api/competiton/:id", (req, res, next) => {
+     var data = {
+       competitonName: req.body.competitonName,
+       competitonid: req.body.competitonid
+     
+     }
+     db.run(
+       `UPDATE competiton set 
+            competitonName = coalesce(?,competitonName), 
+             WHERE questId = ?`,
+       [data.competitonName, data.competitonId, req.params.competitonId],
+       (err, result) => {
+         if (err) {
+           res.status(400).json({ "error": res.message })
+           return;
+       }
+         res.json({
+           message: "success",
+           data: data
+         })
+       });
+  })
