c613adb9b0a5f03bd84e300efd683c3f02f6b911
GANDE RAJASHEKAR <rajshekar2641@gmail.com>
2020-10-23 22:16:33 -0500

    Added the edit contoller

--- controllers/competition/competitionEditController.js
+++ controllers/competition/competitionEditController.js
@@ -8,4 +8,21 @@
  * /*
 *  @author Sumanth Reddy and Rajashekar
 */
- 
+const express=require('express')
+const app=express.Router()
+const Model=require('../../models/competition.js')
+
+app.post("/update", (req, res, next) => {
+    var errors = []
+    if (!req.body.competitionname) {
+      errors.push("Competititon name not specified");
+    }   
+    if (errors.length) {
+      res.status(400).json({ "error": errors.join(",") });
+      return;
+    }
+    res.json({
+      "message": "success"
+    })
+});
+module.exports = app;
