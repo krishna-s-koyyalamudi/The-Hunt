90287d61df44745015379d64b7431dcfc1f834d6
Rajeshwari_Rudra <rajrudra.raj@gmail.com>
2020-10-27 22:33:55 -0400

    add edit.js

---
+++ controllers/location/locationEditController.js
@@ -0,0 +1,39 @@
+/** 
+* @author Rajeshwari Rudravaram
+*
+*
+*/
+
+// const express = require('express');
+// const app = express.Router();
+ 
+// const bodyParser = require('body-parser');
+// app.use(bodyParser.urlencoded({ extended: true }));
+// var jsonParser = bodyParser.json()
+ 
+//  app.patch("/api/location/:id", (req, res, next) => {
+//     var data = {
+//       locationId: req.body.locationId,
+//       questId: req.body.questId,
+//       locationLatitude: req.body.locationLatitude,
+//       locationLongitude: req.body.locationLongitude,
+//       locationValue: req.body.locationValue
+//     }
+//     db.run(
+//       `UPDATE location set 
+//            locationLatitude = COALESCE(?,locationLatitude)
+//            locationLongitude = COALESCE(?,locationLongitude) 
+//            locationValue = COALESCE(?,locationValue) 
+//            WHERE locationId = ?`,
+//       [data.locationId, data.questId, data.locationLatitude, data.locationLongitude, req.params.locationId, req.param.questId],
+//       (err, result) => {
+//         if (err) {
+//           res.status(400).json({ "error": res.message })
+//           return;
+//         }
+//         res.json({
+//           message: "success",
+//           data: data
+//         })
+//       });
+//   })
