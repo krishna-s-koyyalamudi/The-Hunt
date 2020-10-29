7585c1935357a7277f863f83f0baaddc8a68c8ba
GANDE RAJASHEKAR <rajshekar2641@gmail.com>
2020-10-29 02:04:57 -0500

    updated player.js in models

---
+++ models/player.js
@@ -0,0 +1,60 @@
+/**
+*  competition model
+*  explains each attribute in a competition.
+*
+* @author Rajashekar Gande
+*
+*/
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+    sequelize.define('competitionTeamMapping', {
+        playerId: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            notEmpty: true
+        },
+        teamId: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: "team",
+                foreignKey: "teamId"
+            }
+        },
+        userId: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: "user",
+                foreignKey: "userId"
+            }
+        },
+       
+        datetimeInvited: {
+            type: DataTypes.DATE,
+            get() {
+                return moment(this.getDataValue('datetimeInvited')).format('MM/DD/YYYY hh:mm:ss');
+            }
+        },
+        datetimeAccepted: {
+            type: DataTypes.DATE,
+            get() {
+                return moment(this.getDataValue('datetimeAccepted')).format('MM/DD/YYYY hh:mm:ss');
+            }
+        },
+        datetimeRejected: {
+            type: DataTypes.DATE,
+            get() {
+                return moment(this.getDataValue('datetimeRejected')).format('MM/DD/YYYY hh:mm:ss');
+            }
+        },
+        datetimeDeleted: {
+            type: DataTypes.DATE,
+            get() {
+                return moment(this.getDataValue('datetimeDeleted')).format('MM/DD/YYYY hh:mm:ss');
+            }
+        },
+    });
+    // player.associate = function (models) {
+        // associations can be defined here
+    // };
+    //    return player;
+};
