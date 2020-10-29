ed590441844ce21af3e72fffc922842deb899dde
KHARIKA17 <60010885+KHARIKA17@users.noreply.github.com>
2020-10-28 20:15:09 -0500

    edited clue edit controller

--- controllers/clue/clueEditController.js
+++ controllers/clue/clueEditController.js
@@ -5,3 +5,13 @@
  * @author Pooja 
  * @author Harika
  */
+
+//const express=require('express')
+//const app=express.Router()
+//const Model=require('../../models/clue.js')
+//exports.saveEdit = async (req, res) => {
+//try {
+//    const { clueId } = req.params.id;
+//    const [updated] = await db.models.clue.update(req.body, {
+//      where: { id: clueId },
+//    });
