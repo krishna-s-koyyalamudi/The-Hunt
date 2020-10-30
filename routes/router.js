e2211da4170b56c000625b47376aeae24ee11f90
GD-Prasad <g.d.prasad004@gmail.com>
2020-10-30 01:59:57 -0500

    Added routes for delete competition.

--- routes/router.js
+++ routes/router.js
@@ -16,81 +16,84 @@ const sqldb = require('.././database')
 const axios = require('axios')
 var jsonParser = bodyParser.json()
 
-const userLoginController =  require('../controllers/user/Login')
+const userLoginController = require('../controllers/user/Login')
 const userRegisterController = require('../controllers/user/userSignupController')
 
 
 // app.use(bodyParser.json());
 
 router.get('/', (req, res) => {
-  res.render('./user/loginpage', { title: 'Login', layout: false})
+    res.render('./user/loginpage', { title: 'Login', layout: false })
 })
 
 
 
 router.get('/index', (req, res) => {
-  res.render('./user/loginpage', { title: 'Login', layout: false})
+    res.render('./user/loginpage', { title: 'Login', layout: false })
 })
 
 
 
 // Manage top-level request first
 router.get('/Home', (req, res, next) => {
-  res.render('./partials/index', { title: 'The Hunt' })
+    res.render('./partials/index', { title: 'The Hunt' })
 })
 
 router.get('/user/Login', (req, res, next) => {
-  res.render('./user/loginpage', { title: 'Login', layout: false})
+    res.render('./user/loginpage', { title: 'Login', layout: false })
 })
 
 
 router.get('/user/Register', (req, res, next) => {
-  res.render('./user/signuppage', { title: 'Register', layout: false, require: axios})
+    res.render('./user/signuppage', { title: 'Register', layout: false, require: axios })
 })
 
 router.get('/user/forgotcode', (req, res, next) => {
-  res.render('./user/forgotcode', { title: 'Forgot password', layout: false})
+    res.render('./user/forgotcode', { title: 'Forgot password', layout: false })
 })
 
 router.get('/user/forgotemail', (req, res, next) => {
-  res.render('./user/forgotemail', { title: 'Fotgot email', layout: false})
+    res.render('./user/forgotemail', { title: 'Fotgot email', layout: false })
 })
 
 router.get('/user/newPassword', (req, res, next) => {
-  res.render('./user/newpassword', { title: 'New Password', layout: false})
+    res.render('./user/newpassword', { title: 'New Password', layout: false })
 })
 
-router.get('/404' , (req, res, next) => {
-  res.render('./partials/error', { title: 'Error', layout: false})
+router.get('/404', (req, res, next) => {
+    res.render('./partials/error', { title: 'Error', layout: false })
 })
 
 router.get('/create_competition', (req, res, next) => {
-  res.render('./competition/create', { title: 'Register'})
+    res.render('./competition/create', { title: 'Register' })
+})
+router.get('/competition/delete', (req, res, next) => {
+    res.render('./competition/delete', { title: 'Delete Competition' })
 })
 
 router.get('/createScreen', (req, res, next) => {
-  res.render('createScreen', { title: 'Create Screen'})
+    res.render('createScreen', { title: 'Create Screen' })
 })
 
 router.get('/quest/createQuest', (req, res, next) => {
-  res.render('./quest/create', { title: "Quest"})
+    res.render('./quest/create', { title: "Quest" })
 })
 
 router.get('/user/profile', (req, res, next) => {
-  res.render('./player/details', { title: "profile"})
+    res.render('./player/details', { title: "profile" })
 })
 
 
 router.get('/team/createTeam', (req, res, next) => {
-  res.render('./team/create', { title: "Team"})
+    res.render('./team/create', { title: "Team" })
 })
 
 
 router.get('/team/invitePlayers', (req, res, next) => {
-  res.render('./team/invitePlayers', { title: "Invite Players"})
-}) 
+    res.render('./team/invitePlayers', { title: "Invite Players" })
+})
 router.get('/location/delete', (req, res, next) => {
-  res.render('./location/delete', { title: "Locationdelete"})
+    res.render('./location/delete', { title: "Locationdelete" })
 })
 
 // router.use('/api/user/Regitser' , jsonParser, (req, res, next) => {
