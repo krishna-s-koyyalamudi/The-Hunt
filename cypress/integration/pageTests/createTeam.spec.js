0a7ba1a8f0ac1b52a6f02d90891f59cc14404403
sumana-reddy <60016064+sumana-reddy@users.noreply.github.com>
2020-10-30 13:06:48 -0500

    Update createTeam.spec.js
    reverted commit

--- cypress/integration/pageTests/createTeam.spec.js
+++ cypress/integration/pageTests/createTeam.spec.js
@@ -1,37 +1,15 @@
-/**
- * @createscreen.spec.js - cypress testing for create screen
- *
- *
- * @author Sumana Reddy
- */
-/// <reference types="Cypress"/>
-
 Cypress.on('uncaught:exception', (err, runnable) => {
     // returning false here prevents Cypress from
     // failing the test
     return false
   })
-  
+  describe('Create Team page',function(){
+  it('Cypress - Team create page', function () {
+      cy.visit('http://127.0.0.1:3000/createScreen')
+      cy.get('.container').click()
+    cy.visit('http://127.0.0.1:3000/team/createTeam') 
+    cy.get('input').invoke('attr', 'placeholder').should('contain', 'Enter Team Name')
 
-describe('Cypress Test for Create Screen', function(){
-    it('Verify input types &Buttons',function()  {
-      cy.visit('https://hunt2020.herokuapp.com/createScreen')
-     cy.url().should('include','Screen')
-     if(cy.get('#button1').click()){
-         // cy.get('#button1').click();
-    // cy.visit('https://hunt2020.herokuapp.com/createTeam')
-   //   cy.go('forward')
-     cy.go('back')
-     }
-      if(cy.get('button[id="button2"]').click())
-       {
-       
-     cy.visit('https://hunt2020.herokuapp.com/quest')
-     cy.go('forward')
-     cy.go('back')
-     }
-
-     
-    })
-})
-  
+  cy.get('[type="submit"]').click()
+      })
+   }); 
