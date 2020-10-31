2aa882237a9ad16161eeb4a8cf2ec4a0a444c979
Vishal Reddy <59984658+Vishalreddy114@users.noreply.github.com>
2020-10-30 21:41:15 -0500

    updated playerScoreeditController.js

--- controllers/playerscore/playerscoreEditController.js
+++ controllers/playerscore/playerscoreEditController.js
@@ -11,9 +11,9 @@ const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: true }));
 var jsonParser = bodyParser.json()
 
-// app.edit("/playerScore/edit", jsonParser, (req, res, next) => {
-//     console.log(req, " ----request is here")
-//     let playerScoreId = req.body;
+app.edit("/playerScore/edit", jsonParser, (req, res, next) => {
+    console.log(req, " ----request is here")
+    let playerScoreId = req.body;
     
 //  let error = null;
 //     sqldb.get('EDIT FROM playerScore WHERE playerScoreId = ? ', [playerScoreId], (err, response) => {
